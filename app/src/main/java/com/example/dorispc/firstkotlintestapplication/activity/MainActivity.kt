35e20bdb0798d90a67d46d79e42aa0a543776dcf
Doris974 <delphine.guiton2018@campus-eni.fr>
2018-11-06 22:57:46 +0100

    start recyclerview of fideitycards in the main activity

--- app/src/main/java/com/example/dorispc/firstkotlintestapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/dorispc/firstkotlintestapplication/activity/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.dorispc.firstkotlintestapplication.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import android.widget.Toast
 import com.example.dorispc.firstkotlintestapplication.R
+import com.example.dorispc.firstkotlintestapplication.bo.Fidelitycard
 import com.example.dorispc.firstkotlintestapplication.bo.User
+import com.example.dorispc.firstkotlintestapplication.dao.AppDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(){
@@ -18,6 +22,8 @@ class MainActivity : BaseActivity(){
         var user: User = intent.getParcelableExtra("user")
         tbBasic.title = getString(R.string.welcome_firstname,user.firstname)
         setSupportActionBar(tbBasic)
+        rvFidelitydardsMain.layoutManager = LinearLayoutManager(this)
+
 
     }
 
@@ -26,5 +32,19 @@ class MainActivity : BaseActivity(){
             val intent = Intent(context, MainActivity::class.java)
             return intent
         }
+    }
+
+    inner class Worker : AsyncTask<Void,Void,List<Fidelitycard>>(){
+        override fun doInBackground(vararg params: Void?): List<Fidelitycard>? {
+            return AppDatabase.getInstance(this@MainActivity)?.fidelitycardDao()?.getAll()
+        }
+
+        override fun onPostExecute(result: List<Fidelitycard>?) {
+            callBackFidelitycardsList(result)
+        }
+    }
+
+    private fun callBackFidelitycardsList(result: List<Fidelitycard>?) {
+        //rvFidelitydardsMain.adapter
     }
 }
---
+++ app/src/main/java/com/example/dorispc/firstkotlintestapplication/adapter/FidelitycardAdapter.kt
@@ -0,0 +1,3 @@
+package com.example.dorispc.firstkotlintestapplication.adapter
+
+class FidelitycardAdapter
